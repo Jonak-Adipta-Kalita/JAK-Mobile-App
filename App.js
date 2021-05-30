8e689ad4bcf0107f34b61b552d5177a7c54447a0
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-05-30 14:40:32 +0530

    moved header layout to homescreen from app

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import 'react-native-gesture-handler';
-import { View, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { FontAwesome5 } from '@expo/vector-icons';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
 
 const Stack = createStackNavigator();
-const Drawer = createDrawerNavigator();
 
 const globalScreenOptions = {
 	headerStyle: { backgroundColor: '#3f7de0' },
@@ -19,38 +16,9 @@ const globalScreenOptions = {
 export default function App({ navigation }) {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator 
-				screenOptions={globalScreenOptions} 
-				initialRouteName="Home"
-			>
-				<Stack.Screen 
-					name="Home" 
-					component={HomeScreen} 
-					options={({ navigation }) => ({
-						title: "Welcome!!",
-						headerLeft: () => (
-							<View>
-								<SafeAreaView style={{ flex: 1 }}>
-									<TouchableOpacity 
-										style={{ alignItems: "flex-start", margin: 16 }} 
-										onPress={navigation.openDrawer}
-									>
-										<FontAwesome5 name="bars" size={24} />
-									</TouchableOpacity>
-								</SafeAreaView>
-							</View>
-						),
-					})} 
-				/>
+			<Stack.Navigator screenOptions={globalScreenOptions} initialRouteName="Home">
+				<Stack.Screen name="Home" component={HomeScreen} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
 };
-
-const styles = StyleSheet.create({
-	text: {
-		color: "#161924",
-		fontSize: 20,
-		fontWeight: "500"
-	}
-});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,61 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { 
+	View, 
+	Text, 
+	Button, 
+	StyleSheet, 
+	SafeAreaView, 
+	TouchableOpacity 
+} from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
 
 export default function HomeScreen({ navigation }) {
+	const login = () => {};
+	useLayoutEffect(() => {
+		navigation.setOptions({
+			title: "Welcome!!",
+			headerStyle: { backgroundColor: "#fff" },
+			headerTitleStyle: { color: "black" },
+			headerTintColor: "black",
+			headerLeft: () => (
+				<SafeAreaView style={{ flex: 1 }}>
+					<TouchableOpacity 
+						style={{ alignItems: "flex-start", margin: 16 }} 
+						onPress={navigation.openDrawer}
+					>
+						<FontAwesome5 name="bars" size={24} />
+					</TouchableOpacity>
+				</SafeAreaView>
+			),
+			headerRight: () => (
+				<SafeAreaView style={{ flex: 1 }}>
+					<TouchableOpacity 
+						style={{ alignItems: "flex-end", margin: 16, justifyContent: "center" }}
+						onPress={login}
+					>
+						<Button 
+							title="Login" 
+						/>
+					</TouchableOpacity>
+				</SafeAreaView>
+			),
+		});
+	}, [navigation]);
 	return (
 		<View>
 			<StatusBar style="auto" />
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	text: {
+		color: "#161924",
+		fontSize: 20,
+		fontWeight: "500"
+	}
+});
