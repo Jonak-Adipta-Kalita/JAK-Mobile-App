a6f9ec645580b3e5d9e23644233c6afe3ec978c2
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-06-05 13:47:43 +0530

    added login buttons to login screen

--- screens/auth/LoginScreen.js
+++ screens/auth/LoginScreen.js
@@ -1,10 +1,19 @@
 import React, { useLayoutEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { View, SafeAreaView, TouchableOpacity } from "react-native";
+import { View, SafeAreaView, TouchableOpacity, StyleSheet } from "react-native";
 import PropTypes from "prop-types";
-import { AntDesign } from "@expo/vector-icons";
+import { AntDesign, MaterialIcons } from "@expo/vector-icons";
+import { GoogleLoginButton, createButton, createSvgIcon } from "react-social-login-buttons";
 
 export default function LoginScreen({ navigation }) {
+	const googleSignIn = () => {};
+	const EmailLoginButton = createButton({
+		text: "Log in with Email",
+		icon: createSvgIcon(() => <MaterialIcons name="email" size={24} />),
+		iconFormat: (name) => `fa fa-${name}`,
+		style: { background: "#fff", color: "black" },
+		activeStyle: { background: "#EFF0EE" }
+	});
     useLayoutEffect(() => {
         navigation.setOptions({
             title: "Login",
@@ -26,6 +35,20 @@ export default function LoginScreen({ navigation }) {
     return (
         <View>
             <StatusBar style="auto" />
+            <View style={styles.container}>
+				<TouchableOpacity
+					style={styles.button}
+					onPress={() => navigation.navigate("LoginWithEmail")}
+				>
+					<EmailLoginButton />
+				</TouchableOpacity>
+				<TouchableOpacity 
+					style={styles.button} 
+					onPress={googleSignIn}
+				>
+					<GoogleLoginButton />
+				</TouchableOpacity>
+            </View>
         </View>
     );
 }
@@ -33,3 +56,13 @@ export default function LoginScreen({ navigation }) {
 LoginScreen.propTypes = {
     navigation: PropTypes.object.isRequired,
 };
+
+const styles = StyleSheet.create({
+    container: {
+		alignItems: "center",
+        justifyContent: "center",
+	},
+    button: {
+		width: 300,
+	},
+});
