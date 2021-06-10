e582f267ea79bb8c12fedec6a6116deecd2b558f
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-06-10 10:51:14 +0530

    added timestamp to notification component

--- components/Notification.js
+++ components/Notification.js
@@ -1,16 +1,34 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Card } from "react-native-elements";
+import moment from "moment";
 import propTypes from "prop-types";
 
-export default function Notification({ title, message }) {
+function titleCase(string) {
+    let sentence = string.toLowerCase().split(" ");
+    for (let i = 0; i < sentence.length; i++) {
+        sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+    }
+
+    return sentence.join(" ");
+}
+
+export default function Notification({ title, message, timestamp }) {
     return (
         <View style={styles.container}>
-            <Card>
+            <Card style={{ position: "relative" }}>
                 <Card.Title>{title}</Card.Title>
                 <Card.Divider />
                 <Text style={{ textAlign: "center", color: "#594d4c" }}>
                     {message}
+                </Text>
+                <br />
+                <Card.Divider />
+                <br />
+                <Text style={{ position: "absolute", bottom: 0, right: 0 }}>
+                    {timestamp
+                        ? titleCase(moment(timestamp.toDate()).fromNow())
+                        : "..."}
                 </Text>
             </Card>
         </View>
@@ -20,6 +38,7 @@ export default function Notification({ title, message }) {
 Notification.propTypes = {
     title: propTypes.object.isRequired,
     message: propTypes.object.isRequired,
+    timestamp: propTypes.object.isRequired,
 };
 
 const styles = StyleSheet.create({
