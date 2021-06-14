87d9cb3240a4c542708bcadcd6f89cfd5007b08a
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-06-14 14:31:41 +0530

    made backend for changing name

--- screens/settings/ChangeNameScreen.js
+++ screens/settings/ChangeNameScreen.js
@@ -3,6 +3,8 @@ import { StatusBar } from "expo-status-bar";
 import { View, StyleSheet, SafeAreaView, TouchableOpacity } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { Input, Button } from "react-native-elements";
+import { db, auth } from "../../firebase";
+import firebase from "firebase";
 import PropTypes from "prop-types";
 
 export default function ChangeNameScreen({ navigation }) {
@@ -10,6 +12,26 @@ export default function ChangeNameScreen({ navigation }) {
     const changeName = () => {
         if (name === "") {
             alert("Please Enter all the Values in the Form!!");
+        } else {
+            auth.currentUser
+                .updateProfile({
+                    displayName: name,
+                })
+                .then(() => {
+                    db.collection("privateNotifications").add({
+                        title: "Name Changed Successfully!!",
+                        message: `Your Name has been Successfully Changed to ${name}!!`,
+                        timestamp:
+                            firebase.firestore.FieldValue.serverTimestamp(),
+                        user: auth?.currentUser?.email,
+                    });
+                })
+                .then(() => {
+                    setName("");
+                    navigation.jumpTo("Home");
+                })
+                .then(() => alert("Your Name is Successfully Changed!!"))
+                .catch((error) => alert(error.message));
         }
     };
     useLayoutEffect(() => {
