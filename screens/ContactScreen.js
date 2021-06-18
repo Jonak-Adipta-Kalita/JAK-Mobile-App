d9b92c8371daf459d347e13cf7b6fae42d519243
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-06-18 12:35:22 +0530

    removed the restrictions from contact screen

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,15 +1,9 @@
 import React, { useLayoutEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import {
-    View,
-    Text,
-    StyleSheet,
-    SafeAreaView,
-    TouchableOpacity,
-} from "react-native";
+import { View, StyleSheet, SafeAreaView, TouchableOpacity } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { Input, Button } from "react-native-elements";
-import { db, auth } from "../firebase";
+import { db } from "../firebase";
 import firebase from "firebase";
 import PropTypes from "prop-types";
 
@@ -73,56 +67,42 @@ export default function ContactScreen({ navigation }) {
     return (
         <View style={styles.container}>
             <StatusBar style="auto" />
-            {auth.currentUser ? (
-                auth.currentUser.emailVerified ? (
-                    <View>
-                        <View style={styles.inputContainer}>
-                            <Input
-                                placeholder="Name"
-                                autoFocus
-                                type="text"
-                                style={styles.inputBar}
-                                value={name}
-                                onChangeText={(text) => setName(text)}
-                            />
-                            <Input
-                                placeholder="Email"
-                                type="email"
-                                style={styles.inputBar}
-                                value={email}
-                                onChangeText={(text) => setEmail(text)}
-                            />
-                            <Input
-                                placeholder="Phone Number (with Country Code)"
-                                type="phone"
-                                style={styles.inputBar}
-                                value={phoneNumber}
-                                onChangeText={(text) => setPhoneNumber(text)}
-                            />
-                            <Input
-                                placeholder="Why do you want to Contact Me?"
-                                type="text"
-                                style={styles.inputBar}
-                                value={message}
-                                onChangeText={(text) => setMessage(text)}
-                            />
-                        </View>
-                        <Button
-                            style={styles.button}
-                            title="Submit"
-                            onPress={submitRequestToContact}
-                        />
-                    </View>
-                ) : (
-                    <View style={{ width: 350, alignItems: "center" }}>
-                        <Text>Verify Your Email to Send Contact Request!!</Text>
-                    </View>
-                )
-            ) : (
-                <View style={{ width: 350, alignItems: "center" }}>
-                    <Text>Login to Send Contact Request!!</Text>
-                </View>
-            )}
+            <View style={styles.inputContainer}>
+                <Input
+                    placeholder="Name"
+                    autoFocus
+                    type="text"
+                    style={styles.inputBar}
+                    value={name}
+                    onChangeText={(text) => setName(text)}
+                />
+                <Input
+                    placeholder="Email"
+                    type="email"
+                    style={styles.inputBar}
+                    value={email}
+                    onChangeText={(text) => setEmail(text)}
+                />
+                <Input
+                    placeholder="Phone Number (with Country Code)"
+                    type="phone"
+                    style={styles.inputBar}
+                    value={phoneNumber}
+                    onChangeText={(text) => setPhoneNumber(text)}
+                />
+                <Input
+                    placeholder="Why do you want to Contact Me?"
+                    type="text"
+                    style={styles.inputBar}
+                    value={message}
+                    onChangeText={(text) => setMessage(text)}
+                />
+            </View>
+            <Button
+                style={styles.button}
+                title="Submit"
+                onPress={submitRequestToContact}
+            />
         </View>
     );
 }
