2237e2cc44370655f2431964d239af76aced03e1
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-10-24 18:54:39 +0530

    formatted

--- src/utils/registerForPushNotifications.js
+++ src/utils/registerForPushNotifications.js
@@ -3,20 +3,21 @@ import Constants from "expo-constants";
 import * as Notifications from "expo-notifications";
 
 export const registerForPushNotificationsAsync = async () => {
-	if (Constants.isDevice) {
-        const { status: existingStatus } = await Notifications.getPermissionsAsync();
-		let finalStatus = existingStatus;
+    if (Constants.isDevice) {
+        const { status: existingStatus } =
+            await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
 
-		if (existingStatus !== 'granted') {
-			const { status } = await Notifications.requestPermissionsAsync();
-			finalStatus = status;
-		}
+        if (existingStatus !== "granted") {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
 
-		if (finalStatus !== 'granted') {
-			return;
-		}
+        if (finalStatus !== "granted") {
+            return;
+        }
 
-		const token = (await Notifications.getExpoPushTokenAsync()).data;
+        const token = (await Notifications.getExpoPushTokenAsync()).data;
 
         return token;
     }
