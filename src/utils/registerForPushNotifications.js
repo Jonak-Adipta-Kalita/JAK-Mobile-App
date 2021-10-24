c27b85e4100d4beafdcfa4a9a60df1d048456c92
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-10-24 18:50:09 +0530

    i am not understanding a single thing about how to make a push notification

--- src/utils/registerForPushNotifications.js
+++ src/utils/registerForPushNotifications.js
@@ -1,24 +1,24 @@
 import { Platform } from "react-native";
 import Constants from "expo-constants";
-import * as Permissions from "expo-permissions";
 import * as Notifications from "expo-notifications";
 
 export const registerForPushNotificationsAsync = async () => {
-    if (Constants.isDevice) {
-        const { existingStatus } = await Permissions.getAsync(
-            Permissions.NOTIFICATIONS
-        );
-        let finalStatus = existingStatus;
-        if (existingStatus !== "granted") {
-            const { status } = await Permissions.askAsync(
-                Permissions.NOTIFICATIONS
-            );
-            finalStatus = status;
-        }
-        if (finalStatus !== "granted") {
-            return;
-        }
-        return (await Notifications.getExpoPushTokenAsync()).data;
+	if (Constants.isDevice) {
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+		let finalStatus = existingStatus;
+
+		if (existingStatus !== 'granted') {
+			const { status } = await Notifications.requestPermissionsAsync();
+			finalStatus = status;
+		}
+
+		if (finalStatus !== 'granted') {
+			return;
+		}
+
+		const token = (await Notifications.getExpoPushTokenAsync()).data;
+
+        return token;
     }
 
     if (Platform.OS === "android") {
