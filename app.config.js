21a820406e79f7feb20bf5156f0a8de291a08d27
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-11-19 18:18:18 +0530

    lets use app.config.js instead of app.json

---
+++ app.config.js
@@ -0,0 +1,51 @@
+const __DEV__ = true;
+const VERSION = "0.0.1";
+
+export default {
+    name: "jak-mobile-app",
+    slug: "JAK-Mobile-App",
+    version: VERSION,
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    splash: {
+        image: "./assets/images/splash.png",
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+    },
+    privacy: "public",
+    githubUrl: "https://github.com/Jonak-Adipta-Kalita/JAK-Mobile-App",
+    updates: {
+        fallbackToCacheTimeout: 0,
+    },
+    assetBundlePatterns: ["**/*"],
+    notification: {
+        icon: "./assets/images/icon.png",
+        color: "#C14580",
+        androidMode: "collapse",
+        iosDisplayInForeground: false,
+        androidCollapsedTitle:
+            "{unread_notifications} new Notifications from JAK Mobile App!!",
+    },
+    ios: {
+        usesAppleSignIn: true,
+        supportsTablet: true,
+        bundleIdentifier: __DEV__
+            ? "host.exp.exponent"
+            : "com.beastnighttv.jakmobileapp",
+        buildNumber: VERSION,
+    },
+    android: {
+        permissions: [],
+        adaptiveIcon: {
+            foregroundImage: "./assets/images/adaptive-icon.png",
+            backgroundColor: "#FFFFFF",
+        },
+        package: __DEV__
+            ? "host.exp.exponent"
+            : "com.beastnighttv.jakmobileapp",
+        googleServicesFile: "./google-services.json",
+    },
+    web: {
+        favicon: "./assets/images/favicon.png",
+    },
+};
