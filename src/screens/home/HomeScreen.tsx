edc9ecebabc188cb1bd0a9db74d681fb93a317a7
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-09-17 16:01:24 +0530

    the text should also be set using a scheme!!

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -105,7 +105,9 @@ const HomeScreen = () => {
                     </Card>
                 </ScrollView>
             ) : (
-                <Text className="text-bold mt-5 self-center text-lg">
+				<Text className={`text-bold mt-5 self-center text-lg ${scheme === "dark"
+                                                ? "text-white"
+                                                : "text-black"}`}>
                     Login or Register to use the Features!!
                 </Text>
             )}
