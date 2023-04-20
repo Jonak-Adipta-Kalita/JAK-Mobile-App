afe1c8e3fbcba9e3e3023ba85720e2daefc96869
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-04-20 22:21:51 +0530

    gotta think about a good design

--- src/screens/GetStartedScreen.tsx
+++ src/screens/GetStartedScreen.tsx
@@ -1,15 +1,39 @@
 import React from "react";
-import { View } from "react-native";
+import { View, useColorScheme, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { DrawerStackNavigationProps } from "../../@types/navigation";
 import StatusBar from "../components/StatusBar";
+import PagerView from "react-native-pager-view";
+import globalStyles from "../globalStyles";
 
 const GetStartedScreen = () => {
     const navigation = useNavigation<DrawerStackNavigationProps>();
+    const scheme = useColorScheme();
 
     return (
-        <View>
+        <View className="flex-1">
             <StatusBar />
+            <PagerView initialPage={0} style={{ flex: 1 }}>
+                <View key="1" className="items-center justify-center space-y-5">
+                    <Text
+                        className={`px-10 text-center text-2xl ${
+                            scheme === "dark" ? "text-white" : "text-black"
+                        }`}
+                        style={[globalStyles.font]}
+                    >
+                        Takes care of all your daily needs
+                    </Text>
+                    <Text
+                        className={`px-10 text-center text-xs ${
+                            scheme === "dark" ? "text-white" : "text-black"
+                        }`}
+                        style={[globalStyles.font]}
+                    >
+                        Get ready to explore a world of convenience with tons of
+                        essential features at your fingertips.
+                    </Text>
+                </View>
+            </PagerView>
         </View>
     );
 };
