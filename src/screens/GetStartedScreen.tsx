21af8f90ee6a6e7c3939fcdb548c773d780f8a5e
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-04-20 22:36:27 +0530

    meh no image is alright

--- src/screens/GetStartedScreen.tsx
+++ src/screens/GetStartedScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, useColorScheme, Text } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { DrawerStackNavigationProps } from "../../@types/navigation";
 import StatusBar from "../components/StatusBar";
@@ -8,32 +8,38 @@ import globalStyles from "../globalStyles";
 
 const GetStartedScreen = () => {
     const navigation = useNavigation<DrawerStackNavigationProps>();
-    const scheme = useColorScheme();
 
     return (
-        <View className="flex-1">
+        <View className="relative flex-1">
             <StatusBar />
-            <PagerView initialPage={0} style={{ flex: 1 }}>
-                <View key="1" className="items-center justify-center space-y-5">
-                    <Text
-                        className={`px-10 text-center text-2xl ${
-                            scheme === "dark" ? "text-white" : "text-black"
-                        }`}
-                        style={[globalStyles.font]}
+            <ImageBackground
+                source={{
+                    uri: "https://i.pinimg.com/564x/26/47/62/2647624ed5ad7274676372e560210249--orange-wallpaper-wallpaper-backgrounds.jpg",
+                }}
+                resizeMode="cover"
+                className="flex-1"
+            >
+                <PagerView initialPage={0} style={{ flex: 1 }}>
+                    <View
+                        key="1"
+                        className="items-center justify-center space-y-5"
                     >
-                        Takes care of all your daily needs
-                    </Text>
-                    <Text
-                        className={`px-10 text-center text-xs ${
-                            scheme === "dark" ? "text-white" : "text-black"
-                        }`}
-                        style={[globalStyles.font]}
-                    >
-                        Get ready to explore a world of convenience with tons of
-                        essential features at your fingertips.
-                    </Text>
-                </View>
-            </PagerView>
+                        <Text
+                            className={`px-10 text-center text-2xl text-gray-50`}
+                            style={[globalStyles.font]}
+                        >
+                            Takes care of all your daily needs
+                        </Text>
+                        <Text
+                            className={`px-10 text-center text-xs text-gray-50`}
+                            style={[globalStyles.font]}
+                        >
+                            Get ready to explore a world of convenience with
+                            tons of essential features at your fingertips.
+                        </Text>
+                    </View>
+                </PagerView>
+            </ImageBackground>
         </View>
     );
 };
