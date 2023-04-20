ba99966097aa2819b474b04a0100140c56df2d5c
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-04-20 18:32:14 +0530

    gonna have no default header from react navigation

--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -1,12 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Appearance } from "react-native";
-import {
-    createStackNavigator,
-    StackNavigationOptions,
-} from "@react-navigation/stack";
+import { createStackNavigator } from "@react-navigation/stack";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { StackScreenParamList } from "../../@types/navigation";
-import ArrowGoBack from "../components/ArrowGoBack";
 
 import GetStartedScreen from "../screens/GetStartedScreen";
 import HomeScreen from "../screens/home/HomeScreen";
@@ -22,34 +17,10 @@ import LoginScreen from "../screens/auth/LoginScreen";
 import RegisterScreen from "../screens/auth/RegisterScreen";
 
 const Stack = createStackNavigator<StackScreenParamList>();
-const colorScheme = Appearance.getColorScheme();
-
-const globalStackScreenOptions: StackNavigationOptions = {
-    headerBackTitle: "Back",
-    headerTitleAlign: "center",
-};
-
-const stackScreenOption1: StackNavigationOptions = {
-    ...globalStackScreenOptions,
-    headerStyle: {
-        backgroundColor: colorScheme === "dark" ? "#000000" : "#fff",
-    },
-    headerTitleStyle: {
-        color: colorScheme === "dark" ? "#fff" : "#000000",
-        fontFamily: "OtomanopeeOne",
-    },
-    headerTintColor: colorScheme === "dark" ? "#fff" : "#000000",
-};
-
-const stackScreenOption2: StackNavigationOptions = {
-    ...globalStackScreenOptions,
-    headerStyle: { backgroundColor: "#3f7de0" },
-    headerTitleStyle: { color: "white", fontFamily: "OtomanopeeOne" },
-    headerTintColor: "white",
-};
 
 const HomeStack = () => {
     const [isFirstLaunch, setIsFirstLaunch] = useState<boolean | null>(null);
+
     useEffect(() => {
         AsyncStorage.getItem("alreadyLaunched").then((value) => {
             if (value === null) {
@@ -60,55 +31,23 @@ const HomeStack = () => {
             }
         });
     }, []);
+
     if (isFirstLaunch === null) {
         return null;
     }
+
     return (
         <Stack.Navigator
-            screenOptions={stackScreenOption1}
             initialRouteName={isFirstLaunch ? "GetStarted" : "Home"}
+            screenOptions={{ headerShown: false }}
         >
             {isFirstLaunch && (
-                <Stack.Screen
-                    name="GetStarted"
-                    component={GetStartedScreen}
-                    options={{
-                        title: "Get Started",
-                        headerStyle: {
-                            backgroundColor: "#3f7de0",
-                        },
-                        headerTitleStyle: {
-                            color: "white",
-                        },
-                        headerTintColor: "white",
-                        headerTitleAlign: "center",
-                    }}
-                />
+                <Stack.Screen name="GetStarted" component={GetStartedScreen} />
             )}
-            <Stack.Screen
-                name="Home"
-                component={HomeScreen}
-                options={{
-                    title: "Welcome",
-                }}
-            />
-            <Stack.Screen
-                name="Notification"
-                component={NotificationScreen}
-                options={{
-                    title: "Your Notifications",
-                    headerLeft: () => <ArrowGoBack />,
-                }}
-            />
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Notification" component={NotificationScreen} />
             <Stack.Group>
-                <Stack.Screen
-                    name="Todo"
-                    component={TodoScreen}
-                    options={{
-                        title: "Todo(s)",
-                        headerLeft: () => <ArrowGoBack />,
-                    }}
-                />
+                <Stack.Screen name="Todo" component={TodoScreen} />
             </Stack.Group>
         </Stack.Navigator>
     );
@@ -117,25 +56,11 @@ const HomeStack = () => {
 const AuthenticationStack = () => {
     return (
         <Stack.Navigator
-            screenOptions={stackScreenOption2}
             initialRouteName="Login"
+            screenOptions={{ headerShown: false }}
         >
-            <Stack.Screen
-                name="Login"
-                component={LoginScreen}
-                options={{
-                    title: "Login",
-                    headerLeft: () => <ArrowGoBack color="white" />,
-                }}
-            />
-            <Stack.Screen
-                name="Register"
-                component={RegisterScreen}
-                options={{
-                    title: "Register",
-                    headerLeft: () => <ArrowGoBack color="white" />,
-                }}
-            />
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
         </Stack.Navigator>
     );
 };
@@ -143,38 +68,19 @@ const AuthenticationStack = () => {
 const SettingsStack = () => {
     return (
         <Stack.Navigator
-            screenOptions={stackScreenOption1}
             initialRouteName="Settings"
+            screenOptions={{ headerShown: false }}
         >
-            <Stack.Screen
-                name="Settings"
-                component={SettingsScreen}
-                options={{
-                    title: "Your Profile",
-                    headerLeft: () => <ArrowGoBack />,
-                }}
-            />
+            <Stack.Screen name="Settings" component={SettingsScreen} />
             <Stack.Group>
-                <Stack.Screen
-                    name="ChangeName"
-                    component={ChangeNameScreen}
-                    options={{
-                        title: "Change your Name",
-                        headerLeft: () => <ArrowGoBack />,
-                    }}
-                />
+                <Stack.Screen name="ChangeName" component={ChangeNameScreen} />
                 <Stack.Screen
                     name="ChangeEmail"
                     component={ChangeEmailScreen}
-                    options={{
-                        title: "Change your Email",
-                        headerLeft: () => <ArrowGoBack />,
-                    }}
                 />
                 <Stack.Screen
                     name="ChangePhoneNumber"
                     component={ChangePhoneNumberScreen}
-                    options={{ headerLeft: () => <ArrowGoBack /> }}
                 />
             </Stack.Group>
         </Stack.Navigator>
