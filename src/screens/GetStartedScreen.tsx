1e3d59482ebb37bdbb863e3a2ae67657e0bb5f83
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-04-21 16:43:54 +0530

    done with the Onboarding screen?

--- src/screens/GetStartedScreen.tsx
+++ src/screens/GetStartedScreen.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { DrawerStackNavigationProps } from "../../@types/navigation";
 import StatusBar from "../components/StatusBar";
 import PagerView from "react-native-pager-view";
+import { Feather } from "@expo/vector-icons";
 import globalStyles from "../globalStyles";
+import { useColorScheme } from "react-native";
 
 const GetStartedScreen = () => {
     const navigation = useNavigation<DrawerStackNavigationProps>();
+    const colorScheme = useColorScheme();
 
     return (
         <View className="relative flex-1">
@@ -46,7 +49,7 @@ const GetStartedScreen = () => {
                         </Text>
                     </View>
                 </View>
-                <View key="3" className="items-center justify-center">
+                <View key="3" className="relative items-center justify-center">
                     <View className="m-5 space-y-5 rounded-lg bg-cyan-500 p-3 py-10">
                         <Text
                             className={`px-10 text-center text-2xl text-gray-50`}
@@ -62,6 +65,16 @@ const GetStartedScreen = () => {
                             Adipta Kalita)
                         </Text>
                     </View>
+                    <TouchableOpacity
+                        className="absolute bottom-52"
+                        onPress={() => navigation.replace("Home")}
+                    >
+                        <Feather
+                            name="arrow-right-circle"
+                            size={70}
+                            color={colorScheme === "dark" ? "white" : "black"}
+                        />
+                    </TouchableOpacity>
                 </View>
             </PagerView>
         </View>
