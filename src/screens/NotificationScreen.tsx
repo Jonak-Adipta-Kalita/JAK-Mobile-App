e226399f4235eedbd4722883209b4af885f0b02e
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-04-25 21:09:20 +0530

    lets first work on the header....?

--- src/screens/NotificationScreen.tsx
+++ src/screens/NotificationScreen.tsx
@@ -110,18 +110,19 @@ const NotificationScreen = () => {
                         </Text>
                     </View>
                 ) : (
-                    <ScrollView>
-                        {notifications?.docs?.map((notification) => (
-                            <Notification
-                                key={notification.id}
-                                id={notification.id}
-                                title={notification.data().title}
-                                message={notification.data().message}
-                                timestamp={notification.data().timestamp}
-                                canDelete={true}
-                            />
-                        ))}
-                    </ScrollView>
+                    <View></View>
+                    // <ScrollView>
+                    //     {notifications?.docs?.map((notification) => (
+                    //         <Notification
+                    //             key={notification.id}
+                    //             id={notification.id}
+                    //             title={notification.data().title}
+                    //             message={notification.data().message}
+                    //             timestamp={notification.data().timestamp}
+                    //             canDelete={true}
+                    //         />
+                    //     ))}
+                    // </ScrollView>
                 )}
             </View>
         </SafeAreaView>
