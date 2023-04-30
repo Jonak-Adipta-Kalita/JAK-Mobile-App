dd0e7d4e1e1b134faf15c3d1e5b9182e4cc8107a
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-04-30 17:25:45 +0530

    there is a new update in firebase because of which this issue is happening?

--- src/notify/privateNotification.ts
+++ src/notify/privateNotification.ts
@@ -1,8 +1,9 @@
 import { collection, addDoc } from "firebase/firestore";
 import { db } from "../firebase";
 import schedulePushNotification from "../utils/pushNotification/scheduleForPushNotification";
+import { Notification } from "../../@types/data";
 
-const pushPrivateNotification = async (userUID: string, data: any) => {
+const pushPrivateNotification = async (userUID: string, data: Notification) => {
     await schedulePushNotification(data?.title, data?.message, data, userUID);
 
     return addDoc(collection(db, "users", userUID, "notifications"), data);
