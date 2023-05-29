6cd1672c2521090e398e170d7e2b418c80874856
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-05-29 16:33:59 +0530

    now.... i dont know.... what should I do to add limit to the qrcode thing.....

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -95,33 +95,54 @@ const HomeScreen = () => {
         <SafeAreaView className="flex-1">
             <StatusBar />
             {user ? (
-                <View>
-                    <View className="flex flex-row items-center justify-between">
-                        <Text
-                            className={`m-5 mx-10 flex-1 rounded-2xl ${
-                                colorScheme == "dark"
-                                    ? "bg-[#272934] text-gray-200"
-                                    : "bg-white text-gray-900"
-                            } p-2 text-center text-lg`}
-                            style={globalStyles.font}
-                        >
-                            Tools
-                        </Text>
-                    </View>
-                    <ScrollView className="mb-64 mt-10">
-                        <Feature
-                            title="Todo"
-                            description="Create a Todo List"
-                            icon="bars"
-                            buttonOnPress={() => navigation.navigate("Todo")}
-                        />
-                        <Feature
-                            title="QRCode"
-                            description="Create/Scan QRCode"
-                            icon="qrcode"
-                            buttonOnPress={() => navigation.navigate("QRCode")}
-                        />
-                    </ScrollView>
+                <View className="flex-1">
+                    {user.emailVerified ? (
+                        <View>
+                            <View className="flex flex-row items-center justify-between">
+                                <Text
+                                    className={`m-5 mx-10 flex-1 rounded-2xl ${
+                                        colorScheme == "dark"
+                                            ? "bg-[#272934] text-gray-200"
+                                            : "bg-white text-gray-900"
+                                    } p-2 text-center text-lg`}
+                                    style={globalStyles.font}
+                                >
+                                    Tools
+                                </Text>
+                            </View>
+                            <ScrollView className="mb-64 mt-10">
+                                <Feature
+                                    title="Todo"
+                                    description="Create a Todo List"
+                                    icon="bars"
+                                    buttonOnPress={() =>
+                                        navigation.navigate("Todo")
+                                    }
+                                />
+                                <Feature
+                                    title="QRCode"
+                                    description="Create/Scan QRCode"
+                                    icon="qrcode"
+                                    buttonOnPress={() =>
+                                        navigation.navigate("QRCode")
+                                    }
+                                />
+                            </ScrollView>
+                        </View>
+                    ) : (
+                        <View className="flex-1 items-center justify-center">
+                            <Text
+                                className={`self-center rounded-2xl ${
+                                    colorScheme == "dark"
+                                        ? "bg-[#272934] text-gray-200"
+                                        : "bg-white text-gray-900"
+                                } mx-10 p-5 text-center text-lg`}
+                                style={globalStyles.font}
+                            >
+                                Verify your Email to use the Features.
+                            </Text>
+                        </View>
+                    )}
                 </View>
             ) : (
                 <View className="flex-1 items-center justify-center">
