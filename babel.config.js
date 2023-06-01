aea0ae09d85c6371f864b44a7c06ae1458d0340c
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-06-01 15:43:56 +0530

    hmm, babel is doing transformation for react native

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,21 @@ module.exports = function (api) {
         presets: ["babel-preset-expo"],
         plugins: [
             [
+                "module-resolver",
+                {
+                    alias: {
+                        "@/*": ["./*"],
+                        "@components/*": ["./src/components/*"],
+                        "@hooks/*": ["./src/hooks/*"],
+                        "@navigation/*": ["./src/navigation/*"],
+                        "@screens/*": ["./src/screens/*"],
+                        "@themes/*": ["./src/themes/*"],
+                        "@utils/*": ["./src/utils/*"],
+                        "@assets/*": ["./assets/*"],
+                    },
+                },
+            ],
+            [
                 "module:react-native-dotenv",
                 {
                     moduleName: "@env",
