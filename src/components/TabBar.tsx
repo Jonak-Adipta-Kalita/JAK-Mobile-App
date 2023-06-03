e27b0b4c14a514801402e110737d018e27451a22
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-06-03 22:44:51 +0530

    hmm, time to animate? but Idk how to....

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,8 +1,13 @@
-import { BottomTabBar, BottomTabBarProps } from "@react-navigation/bottom-tabs";
 import React from "react";
+import { BottomTabBar, BottomTabBarProps } from "@react-navigation/bottom-tabs";
+import Animated from "react-native-reanimated";
 
 const TabBar = (props: BottomTabBarProps) => {
-    return <BottomTabBar {...props} />;
+    return (
+        <Animated.View>
+            <BottomTabBar {...props} />
+        </Animated.View>
+    );
 };
 
 export default TabBar;
