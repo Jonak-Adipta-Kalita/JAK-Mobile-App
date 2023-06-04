1b2089eb4f3c3a1a57a400d5e1553130223d18d0
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-06-04 19:25:06 +0530

    that works so well :)

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,135 +1,29 @@
-import React, { useRef, useEffect, useState, useContext } from "react";
-import {
-    BottomTabBar,
-    BottomTabBarHeightCallbackContext,
-    BottomTabBarProps,
-} from "@react-navigation/bottom-tabs";
-import {
-    Animated,
-    LayoutChangeEvent,
-    Platform,
-    StyleProp,
-    StyleSheet,
-    useWindowDimensions,
-} from "react-native";
-import { ViewStyle } from "react-native";
+import React, { useState } from "react";
+import { BottomTabBar, BottomTabBarProps } from "@react-navigation/bottom-tabs";
+import { Animated, Easing } from "react-native";
 
-const TabBar = (
-    props: BottomTabBarProps & {
-        style?: Animated.WithAnimatedValue<StyleProp<ViewStyle>>;
-    }
-) => {
-    const focusedRoute = props.state.routes[props.state.index];
-    const focusedDescriptor = props.descriptors[focusedRoute.key];
-    const { tabBarVisibilityAnimationConfig } = focusedDescriptor.options;
+const TabBar = (props: BottomTabBarProps) => {
+    const [isHidden, setIsHidden] = useState(false);
+    const translateY = useState(new Animated.Value(0))[0];
 
-    const dimensions = useWindowDimensions();
-    const onHeightChange = useContext(BottomTabBarHeightCallbackContext);
-
-    const visibilityAnimationConfigRef = useRef(
-        tabBarVisibilityAnimationConfig
-    );
-
-    const [shouldShowTabBar] = useState(true);
-    const [isTabBarHidden, setIsTabBarHidden] = useState(!shouldShowTabBar);
-    const [visible] = useState(
-        () => new Animated.Value(shouldShowTabBar ? 1 : 0)
-    );
-
-    useEffect(() => {
-        visibilityAnimationConfigRef.current = tabBarVisibilityAnimationConfig;
-    });
-
-    useEffect(() => {
-        const visibilityAnimationConfig = visibilityAnimationConfigRef.current;
-
-        if (shouldShowTabBar) {
-            const animation =
-                visibilityAnimationConfig?.show?.animation === "spring"
-                    ? Animated.spring
-                    : Animated.timing;
-
-            animation(visible, {
-                toValue: 1,
-                useNativeDriver: false,
-                duration: 250,
-                ...visibilityAnimationConfig?.show?.config,
-            }).start(({ finished }) => {
-                if (finished) {
-                    setIsTabBarHidden(false);
-                }
-            });
-        } else {
-            setIsTabBarHidden(true);
-
-            const animation =
-                visibilityAnimationConfig?.hide?.animation === "spring"
-                    ? Animated.spring
-                    : Animated.timing;
-
-            animation(visible, {
-                toValue: 0,
-                useNativeDriver: false,
-                duration: 200,
-                ...visibilityAnimationConfig?.hide?.config,
-            }).start();
-        }
-
-        return () => visible.stopAnimation();
-    }, [visible, shouldShowTabBar]);
-
-    const [layout, setLayout] = React.useState({
-        height: 0,
-        width: dimensions.width,
-    });
-
-    const handleLayout = (e: LayoutChangeEvent) => {
-        const { height, width } = e.nativeEvent.layout;
-
-        onHeightChange?.(height);
-
-        setLayout((layout) => {
-            if (height === layout.height && width === layout.width) {
-                return layout;
-            } else {
-                return {
-                    height,
-                    width,
-                };
-            }
-        });
+    const toggleTabBar = () => {
+        setIsHidden(!isHidden);
+        Animated.timing(translateY, {
+            toValue: isHidden ? 0 : 100,
+            duration: 300,
+            easing: Easing.linear,
+            useNativeDriver: true,
+        }).start();
     };
 
     return (
         <Animated.View
-            style={{
-                transform: [
-                    {
-                        translateY: visible.interpolate({
-                            inputRange: [0, 1],
-                            outputRange: [
-                                layout.height +
-                                    Math.max(
-                                        props.insets.bottom -
-                                            Platform.select({
-                                                ios: 4,
-                                                default: 0,
-                                            }),
-                                        0
-                                    ) +
-                                    StyleSheet.hairlineWidth,
-                                0,
-                            ],
-                        }),
-                    },
-                ],
-
-                position: isTabBarHidden ? "absolute" : (null as any),
-            }}
-            pointerEvents={isTabBarHidden ? "none" : "auto"}
-            onLayout={handleLayout}
+            style={[
+                { transform: [{ translateY }] },
+                { position: "absolute", bottom: 0, left: 0, right: 0 },
+            ]}
         >
-            <BottomTabBar {...props} />
+            {!isHidden && <BottomTabBar {...props} />}
         </Animated.View>
     );
 };
