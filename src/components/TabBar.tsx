59cb165d5bad9a54425a68e430b866f76134153f
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-06-04 12:19:27 +0530

    understandaing and modifying code from react-native https://github.com/react-navigation/react-navigation/blob/main/packages/bottom-tabs/src/views/BottomTabBar.tsx#L175-L211

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,8 +1,66 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { BottomTabBar, BottomTabBarProps } from "@react-navigation/bottom-tabs";
-import Animated from "react-native-reanimated";
+import { Animated } from "react-native";
 
 const TabBar = (props: BottomTabBarProps) => {
+    const focusedRoute = props.state.routes[props.state.index];
+    const focusedDescriptor = props.descriptors[focusedRoute.key];
+    const { tabBarVisibilityAnimationConfig } = focusedDescriptor.options;
+
+    const shouldShowTabBar = true;
+
+    const visibilityAnimationConfigRef = useRef(
+        tabBarVisibilityAnimationConfig
+    );
+
+    useEffect(() => {
+        visibilityAnimationConfigRef.current = tabBarVisibilityAnimationConfig;
+    });
+
+    const [visible] = useState(
+        () => new Animated.Value(shouldShowTabBar ? 1 : 0)
+    );
+
+    const [isTabBarHidden, setIsTabBarHidden] = useState(!shouldShowTabBar);
+
+    useEffect(() => {
+        const visibilityAnimationConfig = visibilityAnimationConfigRef.current;
+
+        if (shouldShowTabBar) {
+            const animation =
+                visibilityAnimationConfig?.show?.animation === "spring"
+                    ? Animated.spring
+                    : Animated.timing;
+
+            animation(visible, {
+                toValue: 1,
+                useNativeDriver: false,
+                duration: 250,
+                ...visibilityAnimationConfig?.show?.config,
+            }).start(({ finished }) => {
+                if (finished) {
+                    setIsTabBarHidden(false);
+                }
+            });
+        } else {
+            setIsTabBarHidden(true);
+
+            const animation =
+                visibilityAnimationConfig?.hide?.animation === "spring"
+                    ? Animated.spring
+                    : Animated.timing;
+
+            animation(visible, {
+                toValue: 0,
+                useNativeDriver: false,
+                duration: 200,
+                ...visibilityAnimationConfig?.hide?.config,
+            }).start();
+        }
+
+        return () => visible.stopAnimation();
+    }, [visible, shouldShowTabBar]);
+
     return (
         <Animated.View>
             <BottomTabBar {...props} />
