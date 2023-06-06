508def3b97c51d65d6791784f34a1f485a322952
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-06-06 09:29:11 +0530

    so... time to translate!

--- src/screens/home/features/TranslatorScreen.tsx
+++ src/screens/home/features/TranslatorScreen.tsx
@@ -1,13 +1,48 @@
 import React from "react";
 import StatusBar from "@/src/components/StatusBar";
-import { View } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { Text, View, useColorScheme } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useHideBottomTab } from "@/src/hooks/useBottomTab";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import globalStyles from "@/src/utils/globalStyles";
+import { BottomTabStackNavigationProps } from "@/@types/navigation";
+import { useNavigation } from "@react-navigation/native";
 
 const TranslatorScreen = () => {
+    const navigation =
+        useNavigation<BottomTabStackNavigationProps<"Translator">>();
+    const colorScheme = useColorScheme();
+    useHideBottomTab();
+
     return (
         <SafeAreaView className="flex-1">
             <StatusBar />
-            <View className="flex-1"></View>
+            <View className="flex-1">
+                <View className="flex flex-row items-center justify-between">
+                    <TouchableOpacity
+                        style={globalStyles.headerIcon}
+                        onPress={navigation.goBack}
+                        className="-mt-[0.5px] ml-10"
+                    >
+                        <AntDesign
+                            name="back"
+                            size={24}
+                            color={colorScheme === "dark" ? "#fff" : "#000000"}
+                        />
+                    </TouchableOpacity>
+                    <Text
+                        className={`m-5 mx-10 ml-6 flex-1 rounded-2xl ${
+                            colorScheme == "dark"
+                                ? "bg-[#272934] text-gray-200"
+                                : "bg-white text-gray-900"
+                        } p-2 px-0 text-center text-lg`}
+                        style={globalStyles.font}
+                    >
+                        Translator
+                    </Text>
+                </View>
+            </View>
         </SafeAreaView>
     );
 };
