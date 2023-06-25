d791dd2d83854d841940e6cb76099a42cc37b007
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-06-25 15:26:05 +0530

    gotta test tho....

--- src/utils/checkAncestoryDoc.ts
+++ src/utils/checkAncestoryDoc.ts
@@ -1,7 +1,10 @@
 import { User } from "firebase/auth";
-import { doc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import { db } from "./firebase";
 
 export const checkAncestoryDoc = async (user: User) => {
     const documentDoc = doc(db, "users", user?.uid!);
+    if (!(await getDoc(documentDoc)).exists()) {
+        await setDoc(documentDoc, {});
+    }
 };
