92922a3254294543341347f3e6af1db3c16cdaca
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-06-28 20:19:50 +0530

    hmm, some styles are not coming properly.... tf...

--- src/screens/GetStartedScreen.tsx
+++ src/screens/GetStartedScreen.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
-import { View } from "react-native";
+import { Text, View, Image } from "react-native";
 import StatusBar from "@components/StatusBar";
 import { useHideBottomTab } from "@hooks/useBottomTab";
 import { useExitAppBackHandler } from "@hooks/useExitAppBackHandler";
+import { images } from "../utils/image";
+import globalStyles from "../utils/globalStyles";
 
 const GetStartedScreen = () => {
     const [pageNumber, setPageNumber] = useState<number>(1);
@@ -15,13 +17,25 @@ const GetStartedScreen = () => {
             <StatusBar />
             {/* Screen 1 */}
             <View className="flex-1">
-                {/* Content */}
-                <View></View>
-                {/* Directional */}
-                <View className="absoulute bottom-[70px] right-[40px]">
-                    {/* Dots */}
-                    {/* NextButton */}
+                <View className="flex flex-1 items-center justify-center gap-y-7">
+                    <View>
+                        <Image
+                            source={images.onboardingIllustration}
+                            alt="illustration"
+                        />
+                    </View>
+                    <View className="mx-8 space-y-5">
+                        <Text className="text-center text-[22px] leading-[34px] tracking-[-0.8px] text-[#DADADA]">
+                            Welcome to JAK Mobile App
+                        </Text>
+                        <Text className="text-justify text-[14px] font-medium leading-[24px] tracking-[-0.3px] text-white">
+                            Simplifying your digital life with multiple features
+                            in one app, reducing the need for switching and
+                            saving storage space.
+                        </Text>
+                    </View>
                 </View>
+                <View></View>
             </View>
         </View>
     );
---
+++ src/utils/image.ts
@@ -0,0 +1,3 @@
+export const images = {
+    onboardingIllustration: require("../../assets/images/illustration.png"),
+};
