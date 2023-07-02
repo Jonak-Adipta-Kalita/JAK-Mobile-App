a4056d402396a6c8cf69fdfb293724c8600ea27f
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-07-02 15:35:20 +0530

    time to go back to figma :)

--- src/screens/GetStartedScreen.tsx
+++ src/screens/GetStartedScreen.tsx
@@ -53,7 +53,7 @@ const GetStartedScreen = () => {
                             className="flex items-center justify-center space-y-20"
                         >
                             <WithLocalSvg
-                                asset={require("../../assets/images/illustrations/1.svg")}
+                                asset={require(`../../assets/images/illustrations/${screen.id}.svg`)}
                             />
                             <View className="mx-8 space-y-8">
                                 <Text
