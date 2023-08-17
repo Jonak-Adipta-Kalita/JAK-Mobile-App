01dc5b3e7227df513e58181ce6e8e1c8e1e58bff
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-08-17 23:08:47 +0530

    i am hesitating to use Figma

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,17 +1,12 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import { useRecoilValue } from "recoil";
 import { alertDataState } from "../atoms/alertAtom";
 
 const Alert = () => {
     const alertData = useRecoilValue(alertDataState);
 
-    return (
-        <View>
-            <Text>{alertData.data?.title}</Text>
-            <Text>{alertData.data?.message}</Text>
-        </View>
-    );
+    return <View></View>;
 };
 
 export default Alert;
